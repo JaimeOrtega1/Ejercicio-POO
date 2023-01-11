8aae758ad35bd8ab94b4a8383e5a8cce03b3bd3e
JaimeOrtega1 <112290380+JaimeOrtega1@users.noreply.github.com>
2023-01-10 19:59:30 -0500

    Add files via upload

---
+++ Ejercicios POO/entidad/empleado.js
@@ -0,0 +1,16 @@
+class Empleado {
+    constructor(codigo, nombre, apellido, genero, fechaNacimiento, fechaIngreso,salarioBasico){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.fechaNacimiento = fechaNacimiento;
+        this.fechaIngreso = fechaIngreso;
+        this.salarioBasico = salarioBasico;
+
+
+
+
+    }
+    
+}
